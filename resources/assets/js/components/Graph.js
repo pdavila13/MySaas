c692e4d5f40e0396c5770e8d2578c6585ff5e7da
Paolo Davila <pdavila1313@gmail.com>
2016-04-04 19:15:00 +0200

    New branch tinkering

--- resources/assets/js/components/Graph.js
+++ resources/assets/js/components/Graph.js
@@ -1,3 +1,23 @@
-/**
- * Created by pdavila on 29/03/16.
- */
+import Chart from 'chart.js';
+
+export default {
+    template: '<canvas id="barChartDailySales" style="height: auto">',
+
+    ready() {
+        //alert("Hola soc Graph!");
+        var ctx = document.getElementById("barChartDailySales").getContext("2d");
+        var data = {
+            labels: ["day1","day2","day3","day4",],
+            datasets: [ {
+                data: [65,45,32,89],
+                label: "Daily Sales",
+                fillColor: "rgba(220, 220, 220, 0.5)",
+                strokeColor: "rgba(220, 220, 220, 0.8)",
+                pointHighlightFill: "rgba(220, 220, 220, 0.75)",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+            }]
+        }
+
+        var myBarChart = new Chart(ctx).Bar(data);
+    }
+}
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,2 +1,26 @@
 //import Chart from 'chart.js';
+import Vue from 'vue';
+import Graph from './components/Graph'
 
+//var ctx = document.getElementById("barChartDailySales").getContext("2d");
+//var data = {
+//    labels: ["day1","day2","day3","day4",],
+//    datasets: [ {
+//        data: [65,45,32,89],
+//        label: "Daily Sales",
+//        fillColor: "rgba(220, 220, 220, 0.5)",
+//        strokeColor: "rgba(220, 220, 220, 0.8)",
+//        pointHighlightFill: "rgba(220, 220, 220, 0.75)",
+//        pointHighlightStroke: "rgba(220,220,220,1)",
+//    }]
+//}
+//
+//var myBarChart = new Chart(ctx).Bar(data);
+
+new Vue({
+    el: 'body',
+    ready() {
+        //alert("Vue funciona!!");
+    },
+    components: {Graph}
+})
